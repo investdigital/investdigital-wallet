f1f875ede31640d94dfc303ffa4863b4e05e645f
ZhangCrystal <1060699859@qq.com>
2018-01-17 15:41:58 +0800

    change android

--- index.android.js
+++ index.android.js
@@ -25,6 +25,7 @@ export default class TouchWallet extends Component {
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
+          kebjfkehhvf
         </Text>
       </View>
     );
